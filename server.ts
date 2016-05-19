bef7837568d4bac616fda9c0ea2e01c9564019fa
Fabrice Mulumba <fabreezem@gmail.com>
2016-05-18 20:29:44 -0400

    Added Typescript files and GET request for Welcome page

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
-"use strict";
-var express = require('express');
+import * as express from 'express';  
 var app = express();
 var port = 3000;
+
+
 // basic route
-app.get('/', function (req, res) {
-    // res.send('Hello! The API is at http://localhost:' + port + '/api');
-    res.sendfile('./frontend/views/welcome.html');
+app.get('/', function(req, res) {
+   // res.send('Hello! The API is at http://localhost:' + port + '/api');
+    res.sendfile('./frontend/views/welcome.html'); 
 });
+
+
 app.listen(port); //Must state the port used or else the app will not work.
 console.log('Magic happens at http://localhost:' + port); // States the link for the app
